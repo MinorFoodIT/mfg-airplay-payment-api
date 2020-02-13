d794b91b6a9dcfde46040f0fe1a061050c854623
akarat <akrs33@gmail.com>
2020-02-13 14:14:09 +0700

    Dev: add service mongo

--- services/dbClient.js
+++ services/dbClient.js
@@ -93,6 +93,7 @@ const saveSite = async (site_group,site_group_name,site_id,site_number,bu_code) 
                                                         logger.info('[saveSite] error => '+ e.stack);
                                                     })
                             }else{
+                                logger.info('====> client.release()');
                                 client.release();
                             }
                         })
